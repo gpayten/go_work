f6c399fadac08b03b02a16feac362a1de031bb1d
Garry Payten <garrypayten@yahoo.com>
2017-11-19 17:59:52 -0800

    Chapter 5

---
+++ book/chap5/slice2.go
@@ -0,0 +1,19 @@
+package main 
+
+import "fmt"
+
+func main() {
+	elements := make(map[string]string)
+	elements["H"] = "Hydrogen"
+	elements["He"] = "Helium"
+	elements["Li"] = "Lithium"
+	elements["Be"] = "Berlylium"
+	elements["B"] = "Boron"
+	elements["C"] = "Carbon"
+	elements["N"] = "Nitrogen"
+	elements["O"] = "Oxygen"
+	elements["F"] = "Flourine"
+	elements["Ne"] = "Neon"
+
+	fmt.Println(elements["Un"])
+}
